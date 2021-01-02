639f1abd69be1114df79264bad0414dd3ee06d80
crispycookies <crispycookies@users.noreply.github.com>
2021-01-02 11:46:16 +0100

    Müllsprache

--- src/main/java/at/fhhagenberg/sqe/App.java
+++ src/main/java/at/fhhagenberg/sqe/App.java
@@ -2,12 +2,14 @@ package at.fhhagenberg.sqe;
 
 import javafx.application.Application;
 import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import java.util.Vector;
@@ -44,57 +46,48 @@ public class App extends Application {
     public void start(Stage stage) {
         var c_floor_label = new Label("Current Floor:\t");
         var c_door_label = new Label("Door Status:\t" + Door.closed);
+        var c_speed = new Label("Speed:\t"+  String.valueOf(0));
+        var c_manual = new Label("Manual Mode\t");
+
+
 
         var topPanel = new GridPane();
+        var midPanel = new GridPane();
         var bottomPanel = new GridPane();
+
         var allPanel = new GridPane();
 
         var scene = new Scene(allPanel);
 
 
-        ColumnConstraints cc = new ColumnConstraints(
-                90,
-                90,
-                90,
-                Priority.ALWAYS,
-                HPos.LEFT,
-                true);
+        topPanel.add(c_speed, 0, 1);
+        midPanel.add(c_floor_label, 0, 0);
+        midPanel.add(c_door_label, 0, 1);
 
-        ColumnConstraints spacer = new ColumnConstraints(
-                10,
-                10,
-                10,
-                Priority.ALWAYS,
-                HPos.LEFT,
-                true);
-
-
-        topPanel.add(c_floor_label, 0, 0);
-        topPanel.add(c_door_label, 0, 1);
 
         var disabled = new Vector<Integer>();
         disabled.add(2);
 
-        topPanel.add(drawButtons(5, disabled),0,2);
+        midPanel.add(drawButtons(5, disabled),0,2);
 
         var payload = new GridPane();
         var c_payload_label = new Label("Passengers:\t" + String.valueOf(0));
         payload.add(c_payload_label, 0,0);
 
-        topPanel.add(payload, 0,3);
+        bottomPanel.add(payload, 0,0);
 
         var switchButton = new SwitchButton();
         var gridPane = new GridPane();
         gridPane.add(switchButton,0,0);
-        topPanel.add(gridPane, 0,4);
+        topPanel.add(gridPane, 1,4);
+        topPanel.add(c_manual, 0,4);
 
-        bottomPanel.getColumnConstraints().add(spacer);
-        bottomPanel.getColumnConstraints().add(cc);
-        bottomPanel.getColumnConstraints().add(cc);
-
+        allPanel.setHgap(100);
+        allPanel.setVgap(100);
 
         allPanel.add(topPanel, 0, 0);
-        allPanel.add(bottomPanel, 0, 1);
+        allPanel.add(midPanel, 0, 1);
+        allPanel.add(bottomPanel, 0,2);
 
         allPanel.setStyle("-fx-background-color: #fbfbfb");
 
