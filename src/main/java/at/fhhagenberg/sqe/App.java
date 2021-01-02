c55a764ac0bd4f8c122f939737ee236f62a2b131
crispycookies <crispycookies@users.noreply.github.com>
2021-01-02 13:16:19 +0100

    Java is Sondermüll

--- src/main/java/at/fhhagenberg/sqe/App.java
+++ src/main/java/at/fhhagenberg/sqe/App.java
@@ -19,6 +19,8 @@ import java.util.Vector;
  */
 public class App extends Application {
 
+    final String ID_prefix = "App";
+
     public enum Door {
         open,
         closed
@@ -29,7 +31,7 @@ public class App extends Application {
         int pos = 0;
         for(int i = count; i > 0; i--){
             var elevatorButton = new Button(String.valueOf(i));
-            elevatorButton.setId("#Button"+String.valueOf(i));
+            elevatorButton.setId("#"+ID_prefix+"Button"+String.valueOf(i));
             if(disabled.contains(i)) {
                 elevatorButton.setStyle("-fx-border-width: 0; -fx-background-color: #726f68; -fx-stroke-width: 1; -fx-pref-width: 200; -fx-background-radius: 0");
             }else{
@@ -44,40 +46,41 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        var disabled = new Vector<Integer>();
+        disabled.add(2);
+
+        var switchButton = new SwitchButton();
+        var gridPane = new GridPane();
+
         var c_floor_label = new Label("Current Floor:\t");
         var c_door_label = new Label("Door Status:\t" + Door.closed);
         var c_speed = new Label("Speed:\t"+  String.valueOf(0));
         var c_manual = new Label("Manual Mode\t");
 
-
+        c_floor_label.setId("#"+ID_prefix+"FloorLabel");
+        c_door_label.setId("#"+ID_prefix+"DoorLabel");
+        c_speed.setId("#"+ID_prefix+"SpeedLabel");
+        c_manual.setId("#"+ID_prefix+"ManualLabel");
 
         var topPanel = new GridPane();
         var midPanel = new GridPane();
         var bottomPanel = new GridPane();
-
         var allPanel = new GridPane();
-
-        var scene = new Scene(allPanel);
-
 
         topPanel.add(c_speed, 0, 1);
         midPanel.add(c_floor_label, 0, 0);
         midPanel.add(c_door_label, 0, 1);
 
-
-        var disabled = new Vector<Integer>();
-        disabled.add(2);
-
         midPanel.add(drawButtons(5, disabled),0,2);
 
-        var payload = new GridPane();
-        var c_payload_label = new Label("Passengers:\t" + String.valueOf(0));
-        payload.add(c_payload_label, 0,0);
+        var scene = new Scene(allPanel);
+        {
+            var payload = new GridPane();
+            var c_payload_label = new Label("Passengers:\t" + String.valueOf(0));
+            payload.add(c_payload_label, 0,0);
+            bottomPanel.add(payload, 0,0);
+        }
 
-        bottomPanel.add(payload, 0,0);
-
-        var switchButton = new SwitchButton();
-        var gridPane = new GridPane();
         gridPane.add(switchButton,0,0);
         topPanel.add(gridPane, 1,4);
         topPanel.add(c_manual, 0,4);
