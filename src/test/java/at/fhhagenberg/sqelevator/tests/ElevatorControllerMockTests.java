7a714e9f88ba36e9e883e2310c8caaf3100af4ef
tobi <egtobi@gmail.com>
2021-01-12 19:47:23 +0100

    Doesnt work, I give up

--- src/test/java/at/fhhagenberg/sqelevator/tests/ElevatorControllerMockTests.java
+++ src/test/java/at/fhhagenberg/sqelevator/tests/ElevatorControllerMockTests.java
@@ -23,52 +23,52 @@ import java.util.Timer;
 @ExtendWith(MockitoExtension.class)
 public class ElevatorControllerMockTests {
 
-	@Mock
-	IMainViewModel mvvm;
+    @Mock
+    IMainViewModel mvvm;
 
-	@Mock
-	IElevatorWrapper ew;
+    @Mock
+    IElevatorWrapper ew;
 
-	@Mock
-	IFloorsViewModel fmod;
+    @Mock
+    IFloorsViewModel fmod;
 
-	@Test
-	void testConstructor() throws RemoteException {
-		Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
-		Mockito.when(ew.getFloorNum()).thenReturn(5);
-		ElevatorController ec = new ElevatorController(ew, mvvm);
-		Mockito.verify(fmod).setNumberOfFloors(5);
-		Mockito.verify(ew).getElevatorNum();
-	}
+    @Test
+    void testConstructor() throws RemoteException {
+        Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
+        Mockito.when(ew.getFloorNum()).thenReturn(5);
+        ElevatorController ec = new ElevatorController(ew, mvvm);
+        Mockito.verify(fmod).setNumberOfFloors(5);
+        Mockito.verify(ew).getElevatorNum();
+    }
 
-	@Test
-	void testInitFloors_WithError() throws RemoteException {
-		Mockito.when(ew.getFloorNum()).thenThrow(new RemoteException());
-		Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
-		ElevatorController ec = new ElevatorController(ew, mvvm);
-		Mockito.verify(ew).getElevatorNum();
-		Mockito.verify(mvvm).setConnectionState(false);
-	}
+    @Test
+    void testInitFloors_WithError() throws RemoteException {
+        Mockito.when(ew.getFloorNum()).thenThrow(new RemoteException());
+        Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
+        ElevatorController ec = new ElevatorController(ew, mvvm);
+        Mockito.verify(ew).getElevatorNum();
+        Mockito.verify(mvvm).setConnectionState(false);
+    }
 
-	@Test
-	void testInitElevators_WithError() throws RemoteException {
-		Mockito.when(ew.getFloorNum()).thenReturn(5);
-		Mockito.when(ew.getElevatorNum()).thenThrow(new RemoteException());
-		Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
-		ElevatorController ec = new ElevatorController(ew, mvvm);
-		Mockito.verify(ew).getElevatorNum();
-		Mockito.verify(mvvm).setConnectionState(false);
-		Mockito.verify(fmod).setNumberOfFloors(5);
-	}
+    @Test
+    void testInitElevators_WithError() throws RemoteException {
+        Mockito.when(ew.getFloorNum()).thenReturn(5);
+        Mockito.when(ew.getElevatorNum()).thenThrow(new RemoteException());
+        Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
+        ElevatorController ec = new ElevatorController(ew, mvvm);
+        Mockito.verify(ew).getElevatorNum();
+        Mockito.verify(mvvm).setConnectionState(false);
+        Mockito.verify(fmod).setNumberOfFloors(5);
+    }
 
-	@Test
-	void testGetElevatorDoorStatus() throws Exception {
-		Mockito.when(ew.getFloorNum()).thenReturn(5);
-		Mockito.when(ew.getElevatorNum()).thenThrow(new RemoteException());
-		Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
-		ElevatorController ec = new ElevatorController(ew, mvvm);
-		Mockito.verify(ew).getElevatorNum();
-		Mockito.verify(mvvm).setConnectionState(false);
-		Mockito.verify(fmod).setNumberOfFloors(5);
-	}
+    @Test
+    void testGetElevatorDoorStatus() throws Exception {
+        Mockito.when(ew.getFloorNum()).thenReturn(5);
+        Mockito.when(ew.getElevatorNum()).thenThrow(new RemoteException());
+        Mockito.when(mvvm.getFloorsModel()).thenReturn(fmod);
+        ElevatorController ec = new ElevatorController(ew, mvvm);
+        Mockito.verify(ew).getElevatorNum();
+        Mockito.verify(mvvm).setConnectionState(false);
+        Mockito.verify(fmod).setNumberOfFloors(5);
+    }
 }
